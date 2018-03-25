84bc8ab746012c44f6af462f4ca97f50ca743eef
RWhiteDev <contact@rickywhite.net>
2018-03-25 11:14:40 -0400

    Second App Completed

--- 2 - Guess Number Game/program.py
+++ 2 - Guess Number Game/program.py
@@ -1,4 +1,25 @@
-print('-----------------------------')
-print('    Guess the Number Game')
-print('-----------------------------')
+import random
 
+print('------------------------------')
+print('    Guess that Number Game')
+print('------------------------------')
+
+the_number = random.randint(0, 100)
+# Sets an unguessable variable for the guess variable.
+guess = -1
+
+while guess != the_number:
+    # Next two lines go inside the while loop to avoid infinite loop!
+    number_guess = input('Guess the number between 0 - 100: ')
+    guess = int(number_guess)
+
+    # Start of the if/elif/else loop
+    if guess > the_number:
+        print('Too high. Try again.')
+    elif guess < the_number:
+        print('Too low. Try again.')
+    else:
+        print('Congratulations. You win!')
+
+# Game over!
+print('GAME OVER...')
