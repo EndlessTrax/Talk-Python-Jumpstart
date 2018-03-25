6c187b7d4939fbc2d71933a801ce0bf79b16f0ed
RWhiteDev <contact@rickywhite.net>
2018-03-25 13:00:38 -0400

    Second App Updated ot make more personalized.

--- 2 - Guess Number Game/program.py
+++ 2 - Guess Number Game/program.py
@@ -7,6 +7,7 @@ print('------------------------------')
 the_number = random.randint(0, 100)
 # Sets an unguessable variable for the guess variable.
 guess = -1
+name = input('What is your name? ')
 
 while guess != the_number:
     # Next two lines go inside the while loop to avoid infinite loop!
@@ -15,11 +16,11 @@ while guess != the_number:
 
     # Start of the if/elif/else loop
     if guess > the_number:
-        print('Too high. Try again.')
+        print('Sorry {}, that guess was too HIGH.'.format(name))
     elif guess < the_number:
-        print('Too low. Try again.')
+        print('Sorry {}, that guess was too LOW.'.format(name))
     else:
-        print('Congratulations. You win!')
+        print('Congratulations, {}. The number was indeed {}.  You won!'.format(name, the_number))
 
 # Game over!
 print('GAME OVER...')
