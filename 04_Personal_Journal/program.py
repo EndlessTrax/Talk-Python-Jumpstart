ca331ef7f907f5bdefcc06c4545ca8523dc7fc2e
RWhiteDev <contact@rickywhite.net>
2018-04-01 05:55:08 -0400

    Add 4 complete

--- 04_Personal_Journal/program.py
+++ 04_Personal_Journal/program.py
@@ -1,5 +1,7 @@
 import journal
 
+# TODO Add Docstrings to functions.
+
 def main():
     print_header()
     run_event_loop()
@@ -14,11 +16,11 @@ def print_header():
 
 def run_event_loop():
     print('What did you want to do with your Journal?')
-    cmd = None
+    cmd = 'empty'
     journal_name = 'default'
     journal_data = journal.load(journal_name)
 
-    while cmd != 'x':
+    while cmd != 'x' and cmd:
         cmd = input('[L]ist entries, [A]dd entries, E[x]it: ')
         cmd.lower().strip()
 
@@ -26,7 +28,7 @@ def run_event_loop():
             list_entries(journal_data)
         elif cmd == 'a':
             add_entry(journal_data)
-        elif cmd != 'x':
+        elif cmd != 'x' and cmd:
             print("Sorry, I'm not sure what you mean by '{}'".format(cmd))
 
     print('Exiting...')
@@ -45,4 +47,5 @@ def add_entry(data):
     journal.add_entry(text, data)
 
 
-main()
+if __name__ == '__main__':
+    main()
