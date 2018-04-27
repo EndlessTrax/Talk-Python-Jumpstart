cdedea81456f0b25f964832a60c7a88b18949cb8
RWhiteDev <contact@rickywhite.net>
2018-04-27 15:06:40 -0400

    App 8 - Progress after 6 videos.

--- 08_File_Searcher/program.py
+++ 08_File_Searcher/program.py
@@ -1,4 +1,7 @@
 import os
+import collections
+
+SearchResults = collections.namedtuple('search result', 'file, line, text')
 
 
 def main():
@@ -13,7 +16,13 @@ def main():
         print("Sorry, we can't search nothing...")
         return
 
-    search_folders(folder, text)
+    matches = search_folders(folder, text)
+    for m in matches:
+        print('------ MATCH---------')
+        print('File: ' + m.file)
+        print('Line {}'.format(m.line))
+        print('Text' + m.text.strip())
+        print()
 
 
 def print_header():
@@ -35,11 +44,37 @@ def get_folder_from_user():
 
 def get_search_text_from_user():
     text = input('What are you searching for [single phrase only]?')
-    return text
+    return text.lower()
 
 
 def search_folders(folder, text):
-    print("Would print out results for Dir: {} with the search for {}...".format(folder, text))
+    items = os.listdir(folder)
+    all_matches = []
+
+    for item in items:
+        full_item = os.path.join(folder, item)
+        if os.path.isdir(full_item):
+            continue
+
+        match = search_file(full_item, text)
+        all_matches.extend(match)
+
+    return all_matches
+
+
+def search_file(filename, search_text):
+    matches = []
+
+    with open(filename, 'r', encoding='utf-8') as fin:
+
+        line_number = 0
+        for line in fin:
+            line_number += 1
+            if line.lower().find(search_text) >= 0:
+                m = SearchResults(text=line, file=filename, line=line_number)
+                matches.append(m)
+
+        return matches
 
 
 if __name__ == '__main__':
